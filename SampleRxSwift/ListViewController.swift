fbb836dd05082228e90a3d788f269f74a9c082b7
Roy Cruz <royarcruz@gmail.com>
2020-07-28 16:19:12 +0800

    initialize UI

---
+++ SampleRxSwift/ListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ListViewController.swift
+//  SampleRxSwift
+//
+//  Created by Roi Cruz on 7/28/20.
+//  Copyright © 2020 Roy Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
